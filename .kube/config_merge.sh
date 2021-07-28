19a70fe4afbf575a68e6c4eb9d7bec5aa010811c
ryu-sato <32702772+ryu-sato@users.noreply.github.com>
2021-07-28 11:57:18 +0900

    Update config_merge.sh

--- .kube/config_merge.sh
+++ .kube/config_merge.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eux
 # https://medium.com/@jacobtomlinson/how-to-merge-kubernetes-kubectl-config-files-737b61bd517d
 
-confi_dir="~/.kube/sub-config/"
+config_dir=~/.kube/sub-config/
 file=$(tempfile)
-echo $file
 KUBECONFIG=$(find $config_dir -name 'config*' | xargs | tr ' ' ':') kubectl config view --flatten | tee $file
 
